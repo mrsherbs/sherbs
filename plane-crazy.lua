36ef93eb1cbca1f5e2d8c32b15042f0d0a00fe13
mr.sherbs <102490963+mrsherbs@users.noreply.github.com>
2023-07-05 21:28:57 -0400

    Update plane-crazy.lua

--- plane-crazy.lua
+++ plane-crazy.lua
@@ -1,10 +1,20 @@
 local nhook 
 local GC = getgc()
 
+repeat task.wait()
+    until GC
+
 for _, f in ipairs(GC) do 
     if getfenv(f).script == SettingsScript and type(f) == "function" and getinfo(f).name == "destroyvals" then 
         hookfunction(f,function()end)
     end
 end
+
+nhook = hookmetamethod(game, "__namecall", function(self, ...)
+    if getnamecallmethod() == "Kick" or self == RS.Remotes.KickEvent then 
+        return true
+    end 
+    return nhook(self, ...)
+end)
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3c5eab1ddda4aa5b56f664744e796852.lua"))()
