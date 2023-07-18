c63df7d118f1de4d8328ac8d67573a42abf12908
mr.sherbs <102490963+mrsherbs@users.noreply.github.com>
2023-07-18 15:28:18 -0400

    Update plane-crazy.lua

--- plane-crazy.lua
+++ plane-crazy.lua
@@ -1,13 +1,2 @@
-local nhook 
-local GC = getgc()
-
-repeat task.wait()
-    until GC
-
-for _, f in ipairs(GC) do 
-    if getfenv(f).script == SettingsScript and type(f) == "function" and getinfo(f).name == "destroyvals" then 
-        hookfunction(f,function()end)
-    end
-end
-
+--rickje is a skid
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3c5eab1ddda4aa5b56f664744e796852.lua"))()
